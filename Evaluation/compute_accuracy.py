ccdda6f8dee43d2649faec9a943626d4811d4106
dogac.basaran <dogac.basaran@telecom-paristech.fr>
2016-11-18 20:23:53 +0100

    v1.0

---
+++ Evaluation/compute_accuracy.py
@@ -0,0 +1,155 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 12 21:24:33 2016
+
+@author: dbasaran
+"""
+
+import numpy as np
+import json
+import os
+from os import listdir
+
+# Sets the key name for ground_truth dictionary entry, order the sequences in 
+# ascending order of microphone number and record number
+# Example: If sequence = 'mic4_rec2.wav', sequence_ = 'mic1_rec3.wav'
+#           the key_name = 'mic1_rec3.wav mic4_rec2.wav'
+# Input: sequence - First filename
+#        sequence_ - Second filename
+# Output: key_name - Either 'filename filename_' or 'filename_ filename'
+def set_key_name(sequence, sequence_):
+    mic_number = int(sequence[sequence.find('mic')+3:sequence.find('_')])
+    mic_number_ = int(sequence_[sequence_.find('mic')+3:sequence_.find('_')])
+    rec_number = int(sequence[sequence.find('rec')+3:sequence.find('.wav')])
+    rec_number_ = int(sequence_[sequence_.find('rec')+3:sequence_.find('.wav')])
+    
+    if mic_number > mic_number_:
+        key_name = sequence_ + ' ' + sequence
+    elif mic_number == mic_number_:
+        if rec_number > rec_number_:
+            key_name = sequence_ + ' ' + sequence
+        else:
+            key_name = sequence + ' ' + sequence_
+    else:
+        key_name = sequence + ' ' + sequence_
+    return key_name
+    
+def set_relative_offset(key_name, sequence, relative_offset_):
+    if key_name.find(sequence)==0:
+        relative_offset_distance = relative_offset_
+    else:
+        relative_offset_distance = -relative_offset_
+    return relative_offset_distance
+    
+def extract_estimated_pairs(path, offset_estimation_result_filename, coeff=1.):
+    estimations = {}
+    # Start reading the estimation results 
+    f = open(path + '\\' + offset_estimation_result_filename,'r') 
+    for line in f.readlines():
+        estimated_sequences = line.split(" ")
+        sequence = estimated_sequences[0] # The first sequence is current sequence
+        if len(estimated_sequences) > 2: # This means no aligned sequences with the current sequence
+            # For each estimated sequence
+            for sequence_relative_offset in estimated_sequences[1:-1]:
+                # Estimated sequence and its relative offset to current sequence
+                sequence_ = sequence_relative_offset.split(',')[0]
+                relative_offset_ = coeff * float(sequence_relative_offset.split(',')[1])
+                key_name = set_key_name(sequence, sequence_)
+                relative_offset_ = set_relative_offset(key_name, sequence, relative_offset_)
+                if estimations.has_key(key_name)==False:
+                    estimations[key_name] = relative_offset_
+    f.close()
+    return estimations
+
+def compute_accuracy(path, offset_estimation_result_filename, print_results = False):
+    path1 = path[0]
+    path2 = path[1]
+    path3 = path[2]
+    path4 = path[3]
+
+    FN_1 = 0. # False Negative Type 1
+    FN_2 = 0. # False Negative Type 2
+    FP = 0. # False Positive
+    
+    # Read precomputed ground truth dictionary
+    if path1.find('/')!=-1: # Windows based
+        ground_truth = json.load(file(path1 + '\\' + 'ground_truth.txt')) 
+    else: # Linux based
+        ground_truth = json.load(file(path1 + '/' + 'ground_truth.txt')) 
+    
+    # Extract estimated pairs dictionary
+    if offset_estimation_result_filename.find('fingerprint') == -1: 
+        estimations = extract_estimated_pairs(path3, offset_estimation_result_filename)
+    else:
+        estimations = extract_estimated_pairs(path4, offset_estimation_result_filename)
+        
+    # Scan estimations to find FN_2 and FP types of errors
+    tolerance = 10 # The alignment is acceptable in neighborhood of ground truth with amount of tolerance
+    for key, value in estimations.iteritems():
+        if ground_truth.has_key(key):
+            true_relative_offset = ground_truth[key]
+            estimated_relative_offset = value
+            #print(key + ' ' + np.str(true_relative_offset) + ' ' + np.str(estimated_relative_offset) + '   ' + np.str(np.abs(true_relative_offset-estimated_relative_offset)))
+            if np.abs(true_relative_offset - estimated_relative_offset) > tolerance:
+                FN_2+=1.            
+        else:
+            FP+=1.
+    
+    # Scan ground_truth to find FN_1 type of error
+    for key in ground_truth.iterkeys():
+        if estimations.has_key(key) == False:
+            FN_1+=1.
+    
+    # Find number of recordings for each microphone
+    number_of_microphones = 4
+    number_of_recordings = np.zeros(number_of_microphones,dtype='float64')
+    for filename in listdir(path2):
+        mic_number = int(filename[filename.find('mic')+3:filename.find('_')])
+        rec_number = int(filename[filename.find('rec')+3:filename.find('.wav')])
+        if number_of_recordings[mic_number-1]<rec_number:
+            number_of_recordings[mic_number-1] = rec_number
+    
+    # Compute the number of included pairs as total number - excluded number
+    # Note that we assume that the recordings from the same microphone do not
+    # overlap in the algorithm, hence remove those pairs from the total number
+    K = np.sum(number_of_recordings)
+    total_number_of_pairs = K * (K-1)/2
+    excluded_number_of_pairs = 0                 
+    for i in range(len(number_of_recordings)):
+        K_tmp = number_of_recordings[i]
+        excluded_number_of_pairs += K_tmp * (K_tmp-1)/2            
+    
+    # Compute the final evaluation score OMEGA
+    Omega = 1. - (FN_1 + FN_2 + FP)/(total_number_of_pairs-excluded_number_of_pairs)
+    if print_results == True:
+        print("\nThe evaluation results for " + offset_estimation_result_filename)
+        print(('False Negative Type 1 - FN_1 = {0}').format(FN_1))
+        print(('False Negative Type 2 - FN_2 = {0}').format(FN_2))
+        print(('False Positive - FP = {0}').format(FP))
+        print(('Omega = {0}').format(Omega))
+    
+    return (Omega, FN_1, FN_2, FP)  
+
+if __name__ == '__main__':
+    cw_path = os.getcwd();
+    if cw_path.find('/')!=-1:
+        cw_path = cw_path[:cw_path.find('\\Evaluation')]
+        path1 = cw_path + '\\ground_truth'
+        path2 = cw_path + '\\audio_data'
+        path3 = cw_path + '\\SMC_based_alignment\\SMC_offset_estimation_results'
+        path4 = cw_path + '\\Fingerprinting_based_alignment\\fingerprinting_offset_estimation_results'
+    else:
+        cw_path = cw_path[:cw_path.find('/Evaluation')]
+        path1 = cw_path + '/ground_truth'
+        path2 = cw_path + '/audio_data'
+        path3 = cw_path + '/SMC_based_alignment/SMC_offset_estimation_results'
+        path4 = cw_path + '/Fingerprinting_based_alignment/fingerprinting_offset_estimation_results'
+
+    path = [path1, path2, path3, path4]
+
+    #offset_estimation_result_filename = 'offset_estimation_SMC_result_16_11_2016.txt'
+    
+    offset_estimation_result_filename = 'offset_estimation_fingerprinting_thr_10_result_17_11_2016.txt'
+    
+    Omega, FN_1, FN_2, FP = compute_accuracy(path, offset_estimation_result_filename, True) 
+    
---
+++ Evaluation/groundtruth.py
@@ -0,0 +1,162 @@
+# -*- coding: utf-8 -*-
+"""
+Éditeur de Spyder
+
+Ceci est un script temporaire.
+"""
+
+import numpy as np
+import scipy.io.wavfile
+import json
+import os
+from os import listdir
+
+
+# Find the length of each sequence, in addition finds the silence parts in the
+# beginning of each sequence for calibration of the offsets accordingly 
+# Input: path - path of the files
+# Output: filelengths - Type: Dictionary keys: filenames values: lengths of files
+#         silence_in_the_beginning - Type Dictionary keys: filenames values: amount of silence in seconds
+def find_filelengths(path):        
+    filelengths = {}
+    silence_at_the_beginning = {}
+    for filename in listdir(path):
+        Fs, x = scipy.io.wavfile.read(path + '\\' + filename)
+        if np.where(x!=0)[0][0]-1 >=0:
+            silence_at_the_beginning[filename] = np.float64(np.where(x!=0)[0][0]-1)/np.float64(Fs)
+        else:
+            silence_at_the_beginning[filename] = 0.
+        filelengths[filename] = np.float64(x.shape[0])/np.float64(Fs)    
+    return filelengths, silence_at_the_beginning
+
+# Extract offset information of each file from ground truth file GT_090912.xml 
+# and convert length and offset information from seconds to frames (STFT based)
+# Note that this piece of code is unique for the GT_090912.xml file. For other
+# Jiku datasets it has to be modified!!
+# Input: path - path of the GT_090912.xml file
+#        filelengths - dictionary keys: filenames values: lengths of files
+# Output: offset_length_in_frames - Type:List in format "filename-offset-length"
+def extract_offsets(path, filelengths, silence_in_the_beginning, hopsize = 0.02):
+    # cnti is the number of recordings for microphone i
+    cnt1 = 1
+    cnt2 = 1
+    cnt3 = 1
+    cnt4 = 1
+    offset_length_in_frames = []
+    for line in open(path1 + '\\GT_090912.xml','r').readlines():
+        if line.find('offset') != -1:                        
+            # Extract the length of the sequence
+            mic_number = np.int(line.split('_')[2])
+            if mic_number == 0: # represents 1st mic
+                filename = 'mic1_rec' + np.str(cnt1) + '.wav'
+                N = np.floor(filelengths[filename]/hopsize) # Length in frames
+                cnt1+=1
+            elif mic_number == 2: # represents 2nd mic
+                filename = 'mic2_rec' + np.str(cnt2) + '.wav'
+                N = np.floor(filelengths[filename]/hopsize) # Length in frames
+                cnt2+=1
+            elif mic_number == 3: # represents 3rd mic
+                filename = 'mic3_rec' + np.str(cnt3) + '.wav'
+                N = np.floor(filelengths[filename]/hopsize) # Length in frames
+                cnt3+=1
+            elif mic_number == 4: # represents 4th mic
+                filename = 'mic4_rec' + np.str(cnt4) + '.wav'
+                N = np.floor(filelengths[filename]/hopsize) # Length in frames
+                cnt4+=1               
+            
+            # Extract the offset of the sequence
+            tmp_offset = line.split(':') 
+            offset = np.float(tmp_offset[1]) * 3600. + np.float(tmp_offset[2]) * 60. + np.float(tmp_offset[3][:6])
+            offset = offset + silence_in_the_beginning[filename] # Calibration for silence parts
+            offset = np.floor(offset/hopsize) # Convert offset from seconds to frames
+            
+            # Write Offset-Length tuple to the list    
+            offset_length_in_frames.append((filename, offset, N))            
+    return offset_length_in_frames
+
+# Sets the key name for ground_truth dictionary entry, order the sequences in 
+# ascending order of microphone number and record number
+# Example: If filename = 'mic4_rec2.wav', filename_ = 'mic1_rec3.wav'
+#           the key_name = 'mic1_rec3.wav mic4_rec2.wav'
+# Input: filename - First filename
+#        filename_ - Second filename
+# Output: key_name - Either 'filename filename_' or 'filename_ filename'
+def set_key_name(filename, filename_):
+    mic_number = int(filename[filename.find('mic')+3:filename.find('_')])
+    mic_number_ = int(filename_[filename_.find('mic')+3:filename_.find('_')])
+    rec_number = int(filename[filename.find('rec')+3:filename.find('.wav')])
+    rec_number_ = int(filename_[filename_.find('rec')+3:filename_.find('.wav')])
+    
+    if mic_number > mic_number_:
+        key_name = filename_ + ' ' + filename
+    elif mic_number == mic_number_:
+        if rec_number > rec_number_:
+            key_name = filename_ + ' ' + filename
+        else:
+            key_name = filename + ' ' + filename_
+    else:
+        key_name = filename + ' ' + filename_
+    return key_name
+    
+def set_relative_offset(key_name, filename, offset, offset_):
+    if key_name.find(filename)==0:
+        relative_offset_distance = offset_-offset
+    else:
+        relative_offset_distance = offset-offset_
+    return relative_offset_distance
+
+def set_paths():
+    cw_path = os.getcwd();
+    if cw_path.find('/')!=-1:
+        cw_path = cw_path[:cw_path.find('\\Evaluation\\')]
+        path1 = cw_path + '\\ground_truth\\'
+        path2 = cw_path + '\\audio_data\\'
+        wl = 1; # Defines windows based OS
+    else:
+        cw_path = cw_path[:cw_path.find('/Evaluation/')]
+        path1 = cw_path + '/ground_truth/'
+        path2 = cw_path + '/audio_data/'
+        wl = 2; # Defines windows based OS
+        
+    return (path1,path2,wl)
+
+if __name__ == '__main__':
+    
+    path1, path2, wl = set_paths()
+    
+    filelengths, silence_in_the_beginning = find_filelengths(path2)        
+    
+    offset_length_in_frames = extract_offsets(path2, filelengths, silence_in_the_beginning, hopsize = 0.032)        
+    
+    # Finding the overlapping sequences and relative distances between offsets   
+    ground_truth = {}     
+    res = 4 # The amount in frames that overlapping won't be counted
+    f = open(path1 + 'ground_truth_sequence_ordering.txt','w') # write sequence ordering to a txt file
+    for i in range(len(offset_length_in_frames)):
+        f.write(offset_length_in_frames[i][0] + "\n")    
+        
+        filename = offset_length_in_frames[i][0]
+        offset = offset_length_in_frames[i][1]
+        length = offset_length_in_frames[i][2]
+        #ground_truth[filename] = []
+        for j in range(i+1,len(offset_length_in_frames)):
+            filename_ = offset_length_in_frames[j][0]
+            offset_ = offset_length_in_frames[j][1]
+            length_ = offset_length_in_frames[j][2]
+            
+            if offset + length > offset_ + res:
+                key_name = set_key_name(filename, filename_)
+                relative_offset_distance = set_relative_offset(key_name, filename, offset, offset_)
+                ground_truth[key_name] = relative_offset_distance
+                #ground_truth[key_name].append(relative_offset_distance)
+                print(key_name)       
+                print(ground_truth[key_name])
+                #ground_truth[filename].append((filename_, relative_offset_distance))
+            else:
+                break
+        
+        #print(filename)       
+        #print(ground_truth[filename])       
+    json.dump(ground_truth,file(path1 + 'ground_truth.txt','w'))
+    f.close()
+
